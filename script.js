ea2b7084fc276640a4efd5413fdaee03dbb0f43d
jeremie <jeremiebosuandole@gmail.com>
2023-01-18 11:36:02 +0100

    modification du script

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded',function(){
 	
 }
+function addition(x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+      return "Erreur : veuillez entrer deux nombres!";
+    }
+    return x + y;
+  }
